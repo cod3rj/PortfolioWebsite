9f2f2269fbaaed7c17d83a2ebf575de94428ab8d
James Matthew Dela Vega <jmdelavega@dataplus.com.ph>
2023-11-27 22:49:22 +0800

    Added Sidebar Components and Hero Component and Fixed UI styles.

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,19 +1,21 @@
 import './app.scss'
 import Navbar from "../../features/navbar/Navbar.tsx";
+import Hero from "../../features/hero/Hero.tsx";
 
 function App() {
     return (
         <>
-            <section>
+            <section id="Homepage">
                 <Navbar/>
+                <Hero/>
             </section>
-            <section>Parallax</section>
+            <section id="Services">Parallax</section>
             <section>Services</section>
-            <section>Parallax</section>
+            <section id="Portfolio">Parallax</section>
             <section>Portfolio1</section>
             <section>Portfolio2</section>
             <section>Portfolio3</section>
-            <section>Contact</section>
+            <section id="Contact">Contact</section>
         </>
     )
 }
--- src/features/sidebar/Sidebar.tsx
+++ src/features/sidebar/Sidebar.tsx
@@ -1,13 +1,38 @@
+import { motion } from 'framer-motion';
 import './sidebar.scss'
+import SidebarLinks from "./SidebarLinks.tsx";
+import SidebarToggleButton from "./SidebarToggleButton.tsx";
+import {useState} from "react";
+
+const variants = {
+    open: {
+        clipPath: "circle(1200px at 50px 50px)",
+        transition: {
+            type: "spring",
+            stiffness: 20,
+        }
+    },
+    closed: {
+        clipPath: "circle(30px at 50px 50px)",
+        transition: {
+            delay: 0.5,
+            type: "spring",
+            stiffness: 400,
+            damping: 40
+        }
+    }
+}
+
 const Sidebar = () => {
+    const [open, setOpen] = useState(false);
+
     return (
-        <>
-            <div className="sidebar">
-                <div className="bg">
-                    Links
-                </div>
-            </div>
-        </>
+        <motion.div className="sidebar" animate={open ? "open" : "closed"}>
+            <motion.div className="bg" variants={variants}>
+                <SidebarLinks/>
+            </motion.div>
+            <SidebarToggleButton setOpen={setOpen}/>
+        </motion.div>
     )
 }
 
